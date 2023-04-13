5f801184edf821b6cedf7376a23bb6354439c8ed
LeeJuHui <leejh0799@gmail.com>
2023-04-14 00:15:56 +0900

    프로젝트 생성 (#3)

---
+++ src/main/java/com/moyeou/moyeoubackend/MoyeouBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moyeou.moyeoubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoyeouBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoyeouBackendApplication.class, args);
+	}
+
+}
