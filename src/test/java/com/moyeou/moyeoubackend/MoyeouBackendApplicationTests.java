0d05e63f758be3b51124e2d74181824a0425eb9e
zhzzang <leejh0799@gmail.com>
2023-04-14 00:12:26 +0900

    프로젝트 생성

---
+++ src/test/java/com/moyeou/moyeoubackend/MoyeouBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moyeou.moyeoubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoyeouBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
