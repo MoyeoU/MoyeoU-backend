571aba1f9ce80b46e5e3571f267294b4ef8e7df4
LeeJuHui <leejh0799@gmail.com>
2023-04-20 21:09:21 +0900

    feat: 로그인 구현

--- src/main/java/com/moyeou/moyeoubackend/auth/controller/AuthController.java
+++ src/main/java/com/moyeou/moyeoubackend/auth/controller/AuthController.java
@@ -1,24 +1,35 @@
 package com.moyeou.moyeoubackend.auth.controller;
 
+import com.moyeou.moyeoubackend.auth.JwtTokenProvider;
 import com.moyeou.moyeoubackend.auth.controller.request.LoginRequest;
 import com.moyeou.moyeoubackend.auth.controller.response.LoginResponse;
+import com.moyeou.moyeoubackend.auth.controller.response.RefreshResponse;
 import com.moyeou.moyeoubackend.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.java.Log;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/login")
-    public void login(@Valid @RequestBody LoginRequest request) {
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
+        LoginResponse loginResponse = authService.login(request);
+        return ResponseEntity.ok(loginResponse);
+    }
 
-
+    @PostMapping("/refresh")
+    public ResponseEntity<RefreshResponse> refresh(HttpServletRequest request) {
+        String token = jwtTokenProvider.extractToken(request);
+        RefreshResponse refreshResponse = authService.refresh(token);
+        return ResponseEntity.ok(refreshResponse);
     }
 }
--- src/main/java/com/moyeou/moyeoubackend/member/repository/MemberRepository.java
+++ src/main/java/com/moyeou/moyeoubackend/member/repository/MemberRepository.java
@@ -3,6 +3,10 @@ package com.moyeou.moyeoubackend.member.repository;
 import com.moyeou.moyeoubackend.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Boolean existsByEmail(String email);
+
+    Optional<Member> findByEmail(String email);
 }
