8d3d8e08d788ba5446c1667f6cd46e02ef4f67ba
LeeJuHui <leejh0799@gmail.com>
2023-04-20 21:03:33 +0900

    feat: 응답 dto 생성

--- src/main/java/com/moyeou/moyeoubackend/auth/controller/response/LoginResponse.java
+++ src/main/java/com/moyeou/moyeoubackend/auth/controller/response/LoginResponse.java
@@ -1,11 +1,23 @@
 package com.moyeou.moyeoubackend.auth.controller.response;
 
+import com.moyeou.moyeoubackend.member.domain.Member;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class LoginResponse {
-
     private String nickname;
+    private String accessToken;
+    private String refreshToken;
+
+    public static LoginResponse from(Member member, String accessToken, String refreshToken) {
+        return new LoginResponse(
+                member.getNickname(),
+                accessToken,
+                refreshToken
+        );
+    }
 }
