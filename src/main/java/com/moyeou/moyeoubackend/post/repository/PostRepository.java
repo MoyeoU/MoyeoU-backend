5a65b53e260646b9b3b191ee8e15906f214e8340
LeeJuHui <leejh0799@gmail.com>
2023-05-08 18:22:46 +0900

    feat: 게시물 생성 기능 구현

---
+++ src/main/java/com/moyeou/moyeoubackend/post/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.moyeou.moyeoubackend.post.repository;
+
+import com.moyeou.moyeoubackend.post.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
