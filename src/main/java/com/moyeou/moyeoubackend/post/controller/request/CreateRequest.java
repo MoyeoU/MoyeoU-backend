b17d3ae0a3c8edf74147d548ec6758739272d725
LeeJuHui <leejh0799@gmail.com>
2023-05-08 18:22:22 +0900

    feat: 게시물 생성 dto

---
+++ src/main/java/com/moyeou/moyeoubackend/post/controller/request/CreateRequest.java
@@ -0,0 +1,36 @@
+package com.moyeou.moyeoubackend.post.controller.request;
+
+import com.moyeou.moyeoubackend.member.domain.Member;
+import com.moyeou.moyeoubackend.post.domain.Post;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.moyeou.moyeoubackend.post.domain.PostStatus.PROGRESS;
+
+@Getter
+@AllArgsConstructor
+public class CreateRequest {
+    private String title;
+    private Integer headCount;
+    private String operationWay;
+    private String expectedDate;
+    private String estimatedDuration;
+    private List<String> hashtags;
+
+    public Post toEntity(Member host) {
+        return new Post(
+                title,
+                headCount,
+                1,
+                operationWay,
+                expectedDate,
+                estimatedDuration,
+                PROGRESS,
+                host,
+                new ArrayList<>()
+        );
+    }
+}
