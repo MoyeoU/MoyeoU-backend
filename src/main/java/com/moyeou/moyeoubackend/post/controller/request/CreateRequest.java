24fe0a8320f57aeca7b8d950766104c89f2c3200
LeeJuHui <leejh0799@gmail.com>
2023-05-15 23:39:24 +0900

    게시물 생성 dto 수정

--- src/main/java/com/moyeou/moyeoubackend/post/controller/request/CreateRequest.java
+++ src/main/java/com/moyeou/moyeoubackend/post/controller/request/CreateRequest.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,12 +18,25 @@ import static com.moyeou.moyeoubackend.post.domain.PostStatus.PROGRESS;
 @Builder
 @AllArgsConstructor
 public class CreateRequest {
+    @NotEmpty(message = "제목을 입력해주세요")
     private String title;
+
+    @Min(2)
+    @NotNull(message = "모집 인원을 입력해주세요")
     private Integer headCount;
+
+    @NotEmpty(message = "운영 방식을 입력해주세요")
     private String operationWay;
+
+    @NotEmpty(message = "예상 시작일을 입력해주세요")
     private String expectedDate;
+
+    @NotEmpty(message = "예상 기간을 입력해주세요")
     private String estimatedDuration;
+
+    @NotEmpty(message = "내용을 입력해주세요")
     private String content;
+
     private List<String> hashtags;
 
     public Post toEntity(Member host) {
