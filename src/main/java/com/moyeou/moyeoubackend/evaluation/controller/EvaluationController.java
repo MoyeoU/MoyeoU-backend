4854fe0be7caa33a90db374b40ef5a0aa287ea0d
LeeJuHui <leejh0799@gmail.com>
2023-05-18 16:00:19 +0900

    feat: 평가해야 할 스터디원 조회, 평가된 목록 조회 기능 구현

--- src/main/java/com/moyeou/moyeoubackend/evaluation/controller/EvaluationController.java
+++ src/main/java/com/moyeou/moyeoubackend/evaluation/controller/EvaluationController.java
@@ -1,9 +1,28 @@
 package com.moyeou.moyeoubackend.evaluation.controller;
 
+import com.moyeou.moyeoubackend.auth.supports.LoginMember;
+import com.moyeou.moyeoubackend.evaluation.controller.response.EvaluateeResponse;
+import com.moyeou.moyeoubackend.evaluation.controller.response.EvaluationResponse;
+import com.moyeou.moyeoubackend.evaluation.service.EvaluationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class EvaluationController {
+    private final EvaluationService evaluationService;
+
+    @GetMapping("/posts/{postId}/evaluations")
+    public List<EvaluateeResponse> findEvaluateeList(@PathVariable Long postId, @LoginMember Long memberId) {
+        return evaluationService.findEvaluatee(postId, memberId);
+    }
+
+    @GetMapping("/members/{memberId}/evaluations")
+    public List<EvaluationResponse> findEvaluation(@PathVariable Long memberId) {
+        return evaluationService.findEvaluation(memberId);
+    }
 }
--- src/main/java/com/moyeou/moyeoubackend/evaluation/repository/EvaluationRepository.java
+++ src/main/java/com/moyeou/moyeoubackend/evaluation/repository/EvaluationRepository.java
@@ -3,5 +3,8 @@ package com.moyeou.moyeoubackend.evaluation.repository;
 import com.moyeou.moyeoubackend.evaluation.domain.Evaluation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
+    List<Evaluation> findAllByEvaluateeId(Long memberId);
 }
