a9df7cfa3a6a3c6a14b9e36043b2bada7df55d89
LeeJuHui <leejh0799@gmail.com>
2023-08-17 14:51:10 +0900

    초기 데이터 생성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,56 @@
+insert into category(category_id, name) VALUES (1, '어학');
+insert into category(category_id, name) VALUES (2, '프로그래밍');
+insert into category(category_id, name) VALUES (3, '팀프로젝트');
+insert into category(category_id, name) VALUES (4, '자격증');
+insert into category(category_id, name) VALUES (5, '취미/교양');
+insert into category(category_id, name) VALUES (6, '고시/공무원');
+insert into category(category_id, name) VALUES (7, '기타');
+
+insert into hashtag (hashtag_id, name, category_id) values (1, '토익', 1);
+insert into hashtag (hashtag_id, name, category_id) values (2, '토익 스피킹', 1);
+insert into hashtag (hashtag_id, name, category_id) values (3, '토플', 1);
+insert into hashtag (hashtag_id, name, category_id) values (4, '일본어', 1);
+insert into hashtag (hashtag_id, name, category_id) values (5, '중국어', 1);
+insert into hashtag (hashtag_id, name, category_id) values (6, '한자', 1);
+insert into hashtag (hashtag_id, name, category_id) values (7, '영어회화', 1);
+
+insert into hashtag (hashtag_id, name, category_id) values (8, '프론트엔드', 2);
+insert into hashtag (hashtag_id, name, category_id) values (9, '백엔드', 2);
+insert into hashtag (hashtag_id, name, category_id) values (10, '코딩테스트', 2);
+insert into hashtag (hashtag_id, name, category_id) values (11, '모바일', 2);
+insert into hashtag (hashtag_id, name, category_id) values (12, '보안/네트워크', 2);
+insert into hashtag (hashtag_id, name, category_id) values (13, '게임', 2);
+insert into hashtag (hashtag_id, name, category_id) values (14, '하드웨어', 2);
+insert into hashtag (hashtag_id, name, category_id) values (15, '데이터/AI', 2);
+
+insert into hashtag (hashtag_id, name, category_id) values (16, '공과대학', 3);
+insert into hashtag (hashtag_id, name, category_id) values (17, '사회과학대학', 3);
+insert into hashtag (hashtag_id, name, category_id) values (18, '인문대학', 3);
+insert into hashtag (hashtag_id, name, category_id) values (19, '자연과학대학', 3);
+insert into hashtag (hashtag_id, name, category_id) values (20, '경상대학', 3);
+insert into hashtag (hashtag_id, name, category_id) values (21, '약학대학', 3);
+insert into hashtag (hashtag_id, name, category_id) values (22, '농업생명과학대학', 3);
+insert into hashtag (hashtag_id, name, category_id) values (23, '간호대학', 3);
+insert into hashtag (hashtag_id, name, category_id) values (24, '사범대학', 3);
+insert into hashtag (hashtag_id, name, category_id) values (25, '생활과학대학', 3);
+insert into hashtag (hashtag_id, name, category_id) values (26, '예술대학', 3);
+insert into hashtag (hashtag_id, name, category_id) values (27, '수의과대학', 3);
+
+insert into hashtag (hashtag_id, name, category_id) values (28, '컴활', 4);
+insert into hashtag (hashtag_id, name, category_id) values (29, '정보처리기사', 4);
+insert into hashtag (hashtag_id, name, category_id) values (30, '전기기사', 4);
+insert into hashtag (hashtag_id, name, category_id) values (31, '건축기사', 4);
+insert into hashtag (hashtag_id, name, category_id) values (32, '조리기능사', 4);
+insert into hashtag (hashtag_id, name, category_id) values (33, '한능검', 4);
+
+insert into hashtag (hashtag_id, name, category_id) values (34, '독서', 5);
+insert into hashtag (hashtag_id, name, category_id) values (35, '음악', 5);
+insert into hashtag (hashtag_id, name, category_id) values (36, '그림', 5);
+insert into hashtag (hashtag_id, name, category_id) values (37, '운동', 5);
+
+insert into hashtag (hashtag_id, name, category_id) values (38, '임용고시', 6);
+insert into hashtag (hashtag_id, name, category_id) values (39, '간호사', 6);
+insert into hashtag (hashtag_id, name, category_id) values (40, '의사', 6);
+insert into hashtag (hashtag_id, name, category_id) values (41, '행정고시', 6);
+insert into hashtag (hashtag_id, name, category_id) values (42, '외무고시', 6);
+insert into hashtag (hashtag_id, name, category_id) values (43, '공무원', 6);
