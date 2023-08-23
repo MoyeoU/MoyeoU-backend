b4fa0e368428ba30898c189b0def4749ebb662e8
LeeJuHui <leejh0799@gmail.com>
2023-08-23 23:03:56 +0900

    인증되지 않은 요청에 대한 응답 수정

--- src/main/java/com/moyeou/moyeoubackend/auth/supports/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/moyeou/moyeoubackend/auth/supports/CustomAuthenticationEntryPoint.java
@@ -1,6 +1,5 @@
 package com.moyeou.moyeoubackend.auth.supports;
 
-import com.moyeou.moyeoubackend.common.exception.ErrorCode;
 import com.moyeou.moyeoubackend.common.exception.ErrorResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -8,12 +7,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 @RequiredArgsConstructor
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
     private final ObjectMapper objectMapper;
@@ -23,7 +24,7 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
 
-        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.UNAUTHORIZED_ACCESS);
+        ErrorResponse errorResponse = new ErrorResponse("4005", "로그인이 필요합니다.");
         String jsonErrorResponse = objectMapper.writeValueAsString(errorResponse);
         response.getWriter().write(jsonErrorResponse);
     }
