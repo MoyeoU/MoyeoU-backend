bd81c2aee67b42ebe5a09177453b225b39c6660f
LeeJuHui <leejh0799@gmail.com>
2023-09-04 21:18:37 +0900

    알림 확인 테스트 추가

--- src/test/java/com/moyeou/moyeoubackend/post/acceptance/PostAcceptanceTest.java
+++ src/test/java/com/moyeou/moyeoubackend/post/acceptance/PostAcceptanceTest.java
@@ -190,11 +190,12 @@ public class PostAcceptanceTest extends AcceptanceTest {
         findPosts(request).andExpect(jsonPath("$", hasSize(0)));
     }
 
-    @DisplayName("스터디에 참여한다.")
+    @DisplayName("회원이 모집에 신청을 하면 작성자는 알림을 확인해서 신청을 수락하고, 신청자는 알림을 통해 수락을 확인한다")
     @Test
     void attend() throws Exception {
         var host = signUpLogin("example@o.cnu.ac.kr", "pw", "회원1");
         var member = signUpLogin("ex@o.cnu.ac.kr", "password", "회원2");
+        sseConnect(host, member, null);
 
         // host가 게시물 생성
         var post = createPost(host);
@@ -205,17 +206,21 @@ public class PostAcceptanceTest extends AcceptanceTest {
                 .andExpect(status().isCreated())
                 .andExpect(header().string("Location", startsWith(postUri + "/participations/")));
 
-        // 작성자가 신청 수락 전 현재 참여 인원 : 1(작성자)
-        findPost(member, postUri)
-                .andExpect(jsonPath("$.currentCount").value(1));
+        // host의 알림 조회
+        mockMvc.perform(get("/notifications")
+                        .header("Authorization", "Bearer " + host))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].type").value("ATTEND"));
 
-        // 작성자가 신청 수락
+        // host가 신청 수락
         mockMvc.perform(post(uri(participation) + "/accept")
                 .header("Authorization", "Bearer " + host));
 
-        // 현재 참여 인원 : 2
-        findPost(member, postUri)
-                .andExpect(jsonPath("$.currentCount").value(2));
+        // member의 알림 조회
+        mockMvc.perform(get("/notifications")
+                        .header("Authorization", "Bearer " + member))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].type").value("ACCEPT"));
 
         // 내 활동 내역 조회
         mockMvc.perform(get("/members/me")
@@ -239,12 +244,13 @@ public class PostAcceptanceTest extends AcceptanceTest {
                 .andExpect(jsonPath("$.code").value("4007"));
     }
 
-    @DisplayName("작성자가 스터디를 종료하고, 내가 평가해야 할 스터디원 목록을 조회한다.")
+    @DisplayName("작성자가 스터디를 종료하고, 스터디원은 알림을 통해 내가 평가해야 할 스터디원 목록을 조회한다.")
     @Test
     void end() throws Exception {
         var host = signUpLogin("example@o.cnu.ac.kr", "pw", "작성자");
         var member1 = signUpLogin("member1@o.cnu.ac.kr", "password", "회원1");
         var member2 = signUpLogin("member2@o.cnu.ac.kr", "password", "회원2");
+        sseConnect(host, member1, member2);
 
         var response = createPost(host);
         var postUri = uri(response);
@@ -263,6 +269,13 @@ public class PostAcceptanceTest extends AcceptanceTest {
                         .header("Authorization", "Bearer " + host))
                 .andExpect(status().isOk());
 
+        // member1의 알림 조회 (신청 수락 알림, 스터디 종료 알림)
+        mockMvc.perform(get("/notifications")
+                        .header("Authorization", "Bearer " + member1))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].type").value("ACCEPT"))
+                .andExpect(jsonPath("$.[1].type").value("END"));
+
         // member1이 평가해야 할 스터디원 목록 조회
         mockMvc.perform(get(postUri + "/evaluations")
                         .header("Authorization", "Bearer " + member1))
@@ -272,11 +285,13 @@ public class PostAcceptanceTest extends AcceptanceTest {
                 .andExpect(jsonPath("$[1].member.nickname").value("회원2"));
     }
 
-    @DisplayName("댓글을 작성한다")
+    @DisplayName("회원이 댓글을 작성하고, 게시물 작성자가 알림을 확인한다")
     @Test
     void 댓글_작성() throws Exception {
         var member1 = signUpLogin("example@o.cnu.ac.kr", "pw", "회원1");
         var member2 = signUpLogin("ex@o.cnu.ac.kr", "password", "회원2");
+        sseConnect(member1, member2, null);
+
         var response = createPost(member1);
         var postUri = uri(response);
 
@@ -285,6 +300,13 @@ public class PostAcceptanceTest extends AcceptanceTest {
         findPost(member1, postUri)
                 .andExpect(jsonPath("$.comments[0].nickname").value("회원2"))
                 .andExpect(jsonPath("$.comments[0].content").value("하이"));
+
+        // member1의 알림 조회
+        mockMvc.perform(get("/notifications")
+                .header("Authorization", "Bearer " + member1))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].type").value("COMMENT"))
+                .andExpect(jsonPath("$.[0].postTitle").value("JPA 스터디"));
     }
 
     @DisplayName("댓글을 삭제한다")
@@ -292,6 +314,8 @@ public class PostAcceptanceTest extends AcceptanceTest {
     void 댓글_삭제() throws Exception {
         var member1 = signUpLogin("example@o.cnu.ac.kr", "pw", "회원1");
         var member2 = signUpLogin("ex@o.cnu.ac.kr", "password", "회원2");
+        sseConnect(member1, member2, null);
+
         var response = createPost(member1);
         var postUri = uri(response);
 
@@ -315,6 +339,8 @@ public class PostAcceptanceTest extends AcceptanceTest {
     void 댓글_수정() throws Exception {
         var member1 = signUpLogin("example@o.cnu.ac.kr", "pw", "회원1");
         var member2 = signUpLogin("ex@o.cnu.ac.kr", "password", "회원2");
+        sseConnect(member1, member2, null);
+
         var response = createPost(member1);
         var postUri = uri(response);
 
@@ -430,6 +456,8 @@ public class PostAcceptanceTest extends AcceptanceTest {
         var member1 = signUpLogin("example@o.cnu.ac.kr", "pw", "회원1");
         var member2 = signUpLogin("ex@o.cnu.ac.kr", "password", "회원2");
 
+        sseConnect(member1, member2, null);
+
         var post1 = createPost(member1, "코테 스터디", Arrays.asList("코딩테스트")); // post1
         createPost(member2, "코테준비해요!", Arrays.asList("코딩테스트")); // post2
         createPost(member1, "어학 스터디", Arrays.asList("토익", "토플")); // post3
@@ -446,5 +474,17 @@ public class PostAcceptanceTest extends AcceptanceTest {
         mockMvc.perform(post(post5Uri + "/complete")
                         .header("Authorization", "Bearer " + member1))
                 .andExpect(status().isOk());
+    }
+
+    void sseConnect(String token1, String token2, String token3) throws Exception {
+        mockMvc.perform(get("/subscribe")
+                .header("Authorization", "Bearer " + token1));
+        mockMvc.perform(get("/subscribe")
+                .header("Authorization", "Bearer " + token2));
+
+        if (token3 != null) {
+            mockMvc.perform(get("/subscribe")
+                    .header("Authorization", "Bearer " + token3));
+        }
     }
 }
