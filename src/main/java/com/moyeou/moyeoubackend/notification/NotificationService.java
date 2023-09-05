0f77c7ed391e6e44edd106fa397f4c98a180ce19
LeeJuHui <leejh0799@gmail.com>
2023-09-05 23:07:33 +0900

    sendMessage 예외 처리 수정

--- src/main/java/com/moyeou/moyeoubackend/notification/NotificationService.java
+++ src/main/java/com/moyeou/moyeoubackend/notification/NotificationService.java
@@ -3,16 +3,17 @@ package com.moyeou.moyeoubackend.notification;
 import com.moyeou.moyeoubackend.notification.controller.response.NotificationResponse;
 import com.moyeou.moyeoubackend.notification.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class NotificationService {
     private static final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
@@ -26,13 +27,15 @@ public class NotificationService {
         return emitter;
     }
 
-    public void sendMessage(Long id, Object data) {
-        SseEmitter emitter = emitters.get(id);
+    public void sendMessage(Long id, String data) {
         try {
-            emitter.send(SseEmitter.event().id(String.valueOf(id)).name("sse").data(data));
-        } catch (IOException exception) {
-            emitters.remove(id);
-            emitter.completeWithError(exception);
+            SseEmitter emitter = emitters.get(id);
+            SseEmitter.SseEventBuilder sse = SseEmitter.event()
+                    .id(String.valueOf(id))
+                    .name("sse").data(data);
+            emitter.send(sse);
+        } catch (Exception exception) {
+            log.warn("connect x");
         }
     }
 
