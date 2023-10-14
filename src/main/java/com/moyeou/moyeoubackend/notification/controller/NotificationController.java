93596b44370c1a9b5df350d11498d0503d506b43
LeeJuHui <leejh0799@gmail.com>
2023-10-15 06:52:11 +0900

    API 상세 정보 작성

--- src/main/java/com/moyeou/moyeoubackend/notification/controller/NotificationController.java
+++ src/main/java/com/moyeou/moyeoubackend/notification/controller/NotificationController.java
@@ -3,6 +3,8 @@ package com.moyeou.moyeoubackend.notification.controller;
 import com.moyeou.moyeoubackend.auth.supports.LoginMember;
 import com.moyeou.moyeoubackend.notification.controller.response.NotificationResponse;
 import com.moyeou.moyeoubackend.notification.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,14 +15,17 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Notification", description = "알림 관련 API")
 public class NotificationController {
     private final NotificationService notificationService;
 
+    @Operation(summary = "연결")
     @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter subscribe(@LoginMember Long memberId) {
         return notificationService.connect(memberId);
     }
 
+    @Operation(summary = "알림 조회")
     @GetMapping(value = "/notifications")
     public List<NotificationResponse> notifications(@LoginMember Long memberId) {
         return notificationService.checkNotification(memberId);
