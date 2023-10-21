41c2bfe1aa22c651f3703bb519e559f0e894e7d7
LeeJuHui <leejh0799@gmail.com>
2023-10-22 02:03:58 +0900

    dataCleanup시 인증키를 포함하도록 수정

--- src/main/java/com/moyeou/moyeoubackend/support/cleanup/DataCleanupController.java
+++ src/main/java/com/moyeou/moyeoubackend/support/cleanup/DataCleanupController.java
@@ -1,18 +1,31 @@
 package com.moyeou.moyeoubackend.support.cleanup;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Profile("demo")
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "DataCleanup", description = "데이터 삭제 API(demo 서버에서만)")
 public class DataCleanupController {
     private final DataCleanupService dataCleanupService;
 
+    @Value("#{environment.ADMIN_KEY}")
+    private String adminKey;
+
+    @Operation(summary = "모든 데이터 삭제")
     @DeleteMapping("/all-data")
-    public void clean() {
-        dataCleanupService.execute();
+    public String clean(@RequestParam String key) {
+        if (key.equals(adminKey)) {
+            dataCleanupService.execute();
+            return "success";
+        }
+        return "fail";
     }
 }
