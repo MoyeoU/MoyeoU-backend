6a84cb930534a21634e84784ffa66e95768afcd3
LeeJuHui <leejh0799@gmail.com>
2023-10-23 00:56:53 +0900

    Access-Control-Expose-Headers (Location) 추가

--- src/main/java/com/moyeou/moyeoubackend/auth/SecurityConfig.java
+++ src/main/java/com/moyeou/moyeoubackend/auth/SecurityConfig.java
@@ -16,6 +16,9 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @EnableWebSecurity
@@ -58,6 +61,7 @@ public class SecurityConfig {
         configuration.addAllowedOriginPattern("*");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
+        configuration.setExposedHeaders(Collections.singletonList("Location"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
